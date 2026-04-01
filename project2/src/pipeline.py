6034eac8f15fe8c3c597109a165e31536128ad9b
ChristianDelpin <chrisakde@gmail.com>
2026-04-01 11:23:30 -0400

    Added baseline code.

---
+++ project2/src/pipeline.py
@@ -0,0 +1,32 @@
+import requests
+# import pandas as pd
+
+url = 'https://openlibrary.org/'
+
+endpoint = ''
+
+params = ''
+
+# User-Agent is to identify ourselves, increasing our ratelimit from 1 r/s -> 3 r/s.
+# accept is to define the type of response we want to get, meaning `.json()` is not necessary.
+headers = {  # TODO: Give proper name
+    "User-Agent": "MyAppName/1.0 (cad23j@fsu.edu, lad24e@fsu.edu, lk21h@fsu.edu)",
+    "accept": "application/json"
+}
+
+
+# Testing
+
+endpoint = 'isbn/'
+params = '9780544003415'
+fully_qualified_url = url+endpoint+params
+
+response = requests.get(fully_qualified_url, headers=headers)
+print(f"{response.status_code}\n\n{response}")
+
+d=response.json()
+print(d)
+
+# End Testing
+
+
