128295a63281036bae91e57c163f528ac2eab45f
ChristianDelpin <chrisakde@gmail.com>
2026-04-01 12:04:15 -0400

    Testing saving data as a sqlite db.
    Testing saving data as a sqlite db.

--- project2/src/pipeline.py
+++ project2/src/pipeline.py
@@ -1,12 +1,13 @@
 import requests
-# import pandas as pd
+import pandas as pd
+import sqlite3
+import json
+
 
 url = 'https://openlibrary.org/'
-
 endpoint = ''
-
 params = ''
-
+timeout = 10
 # User-Agent is to identify ourselves, increasing our ratelimit from 1 r/s -> 3 r/s.
 # accept is to define the type of response we want to get, meaning `.json()` is not necessary.
 headers = {  # TODO: Give proper name
@@ -21,11 +22,23 @@ endpoint = 'isbn/'
 params = '9780544003415'
 fully_qualified_url = url+endpoint+params
 
-response = requests.get(fully_qualified_url, headers=headers)
+response = requests.get(fully_qualified_url, headers=headers,timeout=timeout)
 print(f"{response.status_code}\n\n{response}")
 
-d=response.json()
-print(d)
+data=response.json()
+print(data)
+
+df = pd.DataFrame([data])
+
+# Convert DataFrame object into a .SQLite file
+for col in df.columns:
+    if df[col].apply(lambda x: isinstance(x, (list, dict))).any():
+        df[col] = df[col].apply(lambda x: json.dumps(x) if isinstance(x, (list, dict)) else x)
+
+
+with sqlite3.connect("../data/raw/test.sqlite") as conn:
+    df.to_sql("testing_table", conn, if_exists="append",index=False)    
+
 
 # End Testing
 
