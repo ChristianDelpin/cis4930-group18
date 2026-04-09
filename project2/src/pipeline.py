5850b530d32406ad36792b369970266b5d819a2c
ChristianDelpin <chrisakde@gmail.com>
2026-04-08 22:59:18 -0400

    Removed unnecessary code.

--- project2/src/pipeline.py
+++ project2/src/pipeline.py
@@ -2,12 +2,6 @@ import sqlite3
 from helper_functions import setup_currency_databases
 def main():
     setup_currency_databases()
-    # conn = sqlite3.connect("project2/data/processed/currencies.db")
-    # cursor = conn.cursor()
-    # cursor.execute("PRAGMA foreign_key_list(countries_by_currency)")
-    # rows = cursor.fetchall()
-    # print(rows)
-    
     
 if __name__ == "__main__":    
     main()
